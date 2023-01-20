5d41cbd82448042363427c25727fa9cbecec7fdb
Václav Brzezina <brzezina.vaclav@dev-extreme.com>
2023-01-20 10:27:16 +0100

    Github pages workflow

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,7 +38,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/EPAM-JS-Competency-center/headless-cms-intro",
+            "https://github.com/EPAM-JS-Competency-center/headless-cms-intro/blob/main",
         },
         blog: false,
         theme: {
